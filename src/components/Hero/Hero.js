ff19e9839a145a3449c7e20db7856be17f69ddb9
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-05-11 16:16:37 +0200

    Columns added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-const Hero = () => (
+const Hero = props => (
     <header className={styles.component}>
-        <h2 className={styles.title}>My first react component</h2>
-        <img className={styles.image}src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"></img>
+        <h2 className={styles.title}>{props.titleText}</h2>
+        <img className={styles.image} src={props.spaceImage}></img>
     </header>
 );
+Hero.PropTypes = {         // PropTypes w komponencie funkcyjnym
+    titleText: PropTypes.node.isRequired,  // typy wlasciwosci https://reactjs.org/docs/typechecking-with-proptypes.html
+};
 
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';  // typy wartości w propsach, properties, typy danych for Java Script
+import Column from '../Column/Column.js';
 
 class List extends React.Component {  // dziedziczy ona z klasy React.Component
+  static PropTypes = {
+    title: PropTypes.node.isRequired,       // zapisujemy obiekt, w którym kluczami są nazwy właściwości komponentów, które mogą być do niego przekazywane.
+    children: PropTypes.node.isRequired,    // .isRequired, jeśli chcemy wyświetlić błąd w konsoli, jeśli komponent nie otrzymał danej właściwości. Jeśli nie dodamy .isRequired, parametr ten będzie opcjonalny.
+    image: PropTypes.string.isRequired,     // why string?
+  }
+  static defaultProps = {                   // domyslna wartosc wlasciwosci. np kiedy nie zostanie podana zadna zawartosc opisu listy 
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} spaceImage={this.props.image}/>
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column columnTitle={this.props.title1} />
+          <Column columnTitle={this.props.title2} />
+          <Column columnTitle={this.props.title2} />
+        </div>
       </section>
     )
   }
