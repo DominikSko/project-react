ba190f9723d1cf3bf2d02fe531ae0adae6acbeb9
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-05-13 16:29:08 +0200

    no titles showing for columns?

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
+import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {  // dziedziczy ona z klasy React.Component
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello world!</h2>
-        <List title={['Things to do ', <sup key='1'>soon!</sup>]} image={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'} >   
-          <p>I'm planning on doing all these things sooner, rather than later!</p>
-        </List>
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2> 
+        <List {...listData} />   
       </main>
     )
   }
 }
 
+// <List {...listData} /> spread operator, który pozwala na rozpakowanie obiektu lub tablicy.
 // nowością jest tylko słowo default. Dzięki niemu, importując App w pliku index.js, możemy pominąć nawiasy klamrowe, czyli nie musimy pisać import {App} from '...';, tak jak do tej pory to robiliśmy.
 export default App;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,15 +3,22 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';  // typy wartości w propsach, properties, typy danych for Java Script
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {  // dziedziczy ona z klasy React.Component
+  state = {
+    columns: this.props.columns || [],  // stan komponentu
+  }
   static propTypes = {
     title: PropTypes.node.isRequired,       // zapisujemy obiekt, w którym kluczami są nazwy właściwości komponentów, które mogą być do niego przekazywane.
-    children: PropTypes.node.isRequired,    // .isRequired, jeśli chcemy wyświetlić błąd w konsoli, jeśli komponent nie otrzymał danej właściwości. Jeśli nie dodamy .isRequired, parametr ten będzie opcjonalny.
+    //children: PropTypes.node.isRequired,    // .isRequired, jeśli chcemy wyświetlić błąd w konsoli, jeśli komponent nie otrzymał danej właściwości. Jeśli nie dodamy .isRequired, parametr ten będzie opcjonalny.
     image: PropTypes.string.isRequired,     // why string?
+    description: PropTypes.node,
+    columns: PropTypes.array,
   }
   static defaultProps = {                   // domyslna wartosc wlasciwosci. np kiedy nie zostanie podana zadna zawartosc opisu listy 
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   }
 
   render() {
@@ -19,12 +26,12 @@ class List extends React.Component {  // dziedziczy ona z klasy React.Component
       <section className={styles.component}>
         <Hero titleText={this.props.title} spaceImage={this.props.image}/>
         <div className={styles.description}>
-          {this.props.children}
+          {ReactHtmlParser(this.props.description)}
         </div>
-        <div className={styles.columns}>
-          <Column columnTitle='Animals' />
-          <Column columnTitle='Plants' />
-          <Column columnTitle='Minerals' />
+        <div className={styles.columns}>              
+          {this.state.columns.map(({key, ...columnProps}) => ( // wykorzystanie wartosci ze stanu, Metoda .map, którą tutaj wykorzystujemy, jest dostępna dla każdej tablicy (array). Służy ona do modyfikacji każdego jej elementu – ale zamiast zmieniać tablicę, na której została wykonana, zwraca nową tablicę ze zmienionymi wartościami.
+            <Column key={key} {...columnProps} />
+          ))}
         </div>
       </section>
     )
