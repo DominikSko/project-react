579bf0194a24831b9558b582562eec770c7dbb8a
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-05-15 16:36:28 +0200

    problem with redux and store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 // <App /> to <App></App> jest to JSX Pełna nazwa tej składni to JavaScript XML. Pozwala na pisanie kodu, który jest bardzo podobny do HTMLa.
-ReactDOM.render(<App />, document.getElementById('app'));
-
+// ReactDOM.render(<App />, document.getElementById('app'));
+// aby nasza aplikacja mogła korzystać z magazynu, musimy zwrappować całą aplikację w komponent Provider, który przed chwilą zaimportowaliśmy
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
