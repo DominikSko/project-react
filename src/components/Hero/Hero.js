6e95edc81bbc91b1313ec5ec2140db3bfa848827
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-05-15 10:03:37 +0200

    fixed Bugs with ESlint

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser'; // Parsowanie kodu HTML, npm install --save react-html-parser
 
 const Hero = props => (
-    <header className={styles.component}>
-        <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
-        <img className={styles.image} src={props.spaceImage}></img>
-    </header>
+  <header className={styles.component}>
+    <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
+    <img className={styles.image} src={props.spaceImage}></img>
+  </header>
 );
 Hero.propTypes = {         // PropTypes w komponencie funkcyjnym
-    titleText: PropTypes.node.isRequired,  // typy wlasciwosci https://reactjs.org/docs/typechecking-with-proptypes.html
+  titleText: PropTypes.node.isRequired,  // typy wlasciwosci https://reactjs.org/docs/typechecking-with-proptypes.html
+  spaceImage: PropTypes.string,
 };
 
 export default Hero;
