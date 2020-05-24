2a35d21cc453826722cd3bae3b7dfa6e03ba48f2
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-05-24 16:53:48 +0200

    Added React transition

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,25 @@ import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import FAQ from '../FAQ/FAQ';
-import MainLayout from '../MainLayout/MainLayout.js';
-// podłączenie routingu w App
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import MainLayout from '../MainLayout/MainLayout.js';   // MainLayout tylko zagnieżdża całą stronę w divie, nowe elementy w main layout dodajemy
+import {BrowserRouter, /*Switch*/ Route} from 'react-router-dom'; // podłączenie routingu w App
+import {AnimatedSwitch} from 'react-router-transition';  // Animowane zmiany widoku
+import styles from './App.scss';
 
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}
+      >
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/FAQ' component={FAQ} />
-      </Switch>
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
